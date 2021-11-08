efd7c37e97647e4723029f1f9262d48d3db9c266
Jorge <joradiazf@gmail.com>
2021-11-08 10:46:47 -0500

    Registro a BD

--- lagenerica/src/main/java/co/edu/lagenerica/controller/ProductosController.java
+++ lagenerica/src/main/java/co/edu/lagenerica/controller/ProductosController.java
@@ -1,13 +1,28 @@
 package co.edu.lagenerica.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public class ProductosController {
+import co.edu.lagenerica.model.Productos;
+import co.edu.lagenerica.model.Tutorial;
+import co.edu.lagenerica.repository.ProductoRepository;
+import co.edu.lagenerica.repository.TutorialRepository;
 
-	@PutMapping("/producto/registrarProducto")
-	public Productos registrarProducto(@RequestBody Productos p) {
-		ProductosDAO dao =  new ProductosDAO();
-		return dao.registrarProducto(p);	
-	}
+public class ProductosController {
+	@Autowired
+	  ProductoRepository productoRepository;
+	
+	@PostMapping("/tutorials")
+	  public ResponseEntity<Productos> createTutorial(@RequestBody Productos producto) {
+		  try {
+			    Productos _producto = productoRepository.save(new Productos(producto.getCodigo(), producto.getNombre(),producto.getNit_proveedor(),producto.getPrecio_compra(),producto.getIva(), producto.getPrecio_venta()));
+			    return new ResponseEntity<>(_producto, HttpStatus.CREATED);
+			  } catch (Exception e) {
+			    return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			  }
+	  }
 }
--- lagenerica/src/main/java/co/edu/lagenerica/model/ProductosDAO.java
+++ lagenerica/src/main/java/co/edu/lagenerica/model/ProductosDAO.java
@@ -5,20 +5,5 @@ import java.sql.Statement;
 
 
 public class ProductosDAO {
-	public Productos registrarProducto(Productos p) {
-		Conexion conex = new Conexion();
-		try {
-			Statement estatuto = conex.getConnection().createStatement();
-			estatuto.executeUpdate("INSERT INTO productos VALUES ('" + p.getCodigo() + "', '" + p.getNitpro()
-					+ "','" + p.getIvacompra() + "', '" + p.getNombre()  + "', '" + p.getPreciocompra()  + "', '" + p.getPrecioventa() + "');");
-
-			estatuto.close();
-			conex.close();
-
-		} catch (SQLException e) {
-			p = null;
-			System.out.println(e.getMessage());
-		}
-		return p;
-	}
+	
 }
