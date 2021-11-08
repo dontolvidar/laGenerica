127747998ec2fce5e0db63739460f786abd322aa
Jorge <joradiazf@gmail.com>
2021-11-08 10:11:37 -0500

    Controler

--- lagenerica/src/main/java/co/edu/lagenerica/repository/TutorialRepository.java
+++ lagenerica/src/main/java/co/edu/lagenerica/repository/TutorialRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import co.edu.lagenerica.model.Tutorial;
 
 public interface TutorialRepository extends MongoRepository<Tutorial, String>{
-	List<Tutorial> findByTitleContaining(String title);
+		List<Tutorial> findByTitleContaining(String title);
 	  List<Tutorial> findByPublished(boolean published);
 }
